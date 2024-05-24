052ac2090664cc4baef3b5a5dddb3a588d6a16d3
marco796 <marcoberlato.music@gmail.com>
2024-05-24 11:44:44 +0200

    ho creato la funzione getComputerChoice

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-function getComputerChoice {};
+//creare una funzione, che randomicamente ritorni rock, paper o scissors//
+ //creare la funzione getComputerChoice//
+    //creare una variante variabile computerChoice, uguale a metodo matematico per avere come risultato o 0 o 1 o 2//
+//creare switch, con argomento computerChoice//
+    //caso computerChoice = 0//
+        //console.log, "Rock!"//
+        //break//
+    //caso computerChoice = 1//
+        //console.log, "Paper!"//
+        //break//
+    //caso computerChoice = 2//
+        //console.log, "Scissors!"//
+        //break//
+//richiamare la funzione//
+
+
+
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    switch (computerChoice) {
+        case 0:
+            console.log("Rock!");
+            break;
+        case 1: 
+            console.log("Paper!");
+            break;
+        case 2:
+            console.log("Scissors!");
+            break;
+    }
+}
+
+getComputerChoice();
