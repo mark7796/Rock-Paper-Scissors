efb828622cf98f2eac6fce9d36ff863ae5367fd4
marco796 <marcoberlato.music@gmail.com>
2024-05-27 11:56:33 +0200

    devo capire che problema ha getHumanChoice

--- index.js
+++ index.js
@@ -13,39 +13,46 @@ function getComputerChoice() {
     }
 }
 
+
 function getHumanChoice() {
     const readline = require("readline").createInterface({
         input: process.stdin,
-        output: process.stout,
+        output: process.stdout,
     });
-    readline.question("What do you choose? rock, paper or scissors?"), humanChoice => {
-        console.log(`${humanChoice}`);
+    readline.question("What do you choose?", humanChoice => {
+        console.log(`Ok, ${humanChoice}!`);
         readline.close();
-    }
+    });
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound (humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
+    if (humanChoice === computerChoice) {
         console.log("Tie!");
     } else if (humanChoice == "rock" && computerChoice == "paper") {
         console.log("You lose!");
+        computerScore++;
     } else if (humanChoice == "rock" && computerChoice == "scissors") {
         console.log("You win!");
+        humanScore++;
     } else if (humanChoice == "paper" && computerChoice == "scissors") {
         console.log("You lose!");
+        computerScore++;
     } else if (humanChoice == "paper" && computerChoice == "rock") {
         console.log("You win!");
+        humanScore++;
     } else if (humanChoice == "scissors" && computerChoice == "paper") {
         console.log("You win!");
+        humanScore++;
     } else if (humanChoice == "scissors" && computerChoice == "rock") {
         console.log("You lose!");
+        computerScore++;
     }
 }
 
-const computerSelection = getComputerChoice();
 const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
