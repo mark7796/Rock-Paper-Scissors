7e4fcee34be28c5668ba94a64c68633da61396dc
marco796 <marcoberlato.music@gmail.com>
2024-05-27 10:34:22 +0200

    ho creato la funzione getHumanChoice. è da capire perchè compare undefined

--- index.js
+++ index.js
@@ -15,4 +15,19 @@ function getComputerChoice() {
     return computerValue;
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    const readline = require('readline');
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('What do you choose? rock, paper or scissors?', (answer) => {
+        console.log(`Your choice is: ${answer}`);
+
+    rl.close();
+    });
+}
+
+console.log(getHumanChoice());
